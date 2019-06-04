d567148046d1d228599cf126c48746aa405c8e71
Rawda Yasser <rawdayasser4@gmail.com>
2019-06-04 10:31:57 +0200

    first commit

---
+++ src/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+		String str = "App is working Fine";
+		assertEquals("App is working Fine", str);
+	}
+
+}
